f675f41e9ef6b61404b126f9494ea5184b7704fb
Jonathan LOQUET <Jonthan.LT@gmail.com>
2018-05-21 23:16:39 +0000

    Change setup and add cron

---
+++ cron.subscribe.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+VIRTUALSKIN_LIMB_ADMIN=`echo $VIRTUALSKIN_LIMB_ADMIN`
+
+* * * * * bash $VIRTUALSKIN_LIMB_ADMIN/subscribe.sh >> $VIRTUALSKIN_LIMB_ADMIN/subscribe.log
--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [ -f /etc/init.d/limb-manager ] ; then
+if [[ -f /etc/init.d/limb-manager ]]; then
     sudo rm /etc/init.d/limb-manager
 fi
 sudo cp limb-manager /etc/init.d/limb-manager
 sudo update-rc.d limb-manager defaults
 sudo systemctl start limb-manager.service
+
+sudo crontab -u pi cron.subscribe.sh
